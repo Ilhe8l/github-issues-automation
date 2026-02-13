04bed1c2e597b6cc97dac3ad48e34108c3bfd455
Guilherme Souza <guilhermesouza3278@gmail.com>
2026-02-13 14:16:48 -0300

    fix: possibilita a url do redis ser pega da .env em update_redis.py

--- update_redis.py
+++ update_redis.py
@@ -1,7 +1,9 @@
 import json
 import redis
+from dotenv import load_dotenv
+load_dotenv()
 
-REDIS_URL = "redis://localhost:6379/0"
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
 def get_redis_client():
     return redis.Redis.from_url(REDIS_URL, decode_responses=True)
